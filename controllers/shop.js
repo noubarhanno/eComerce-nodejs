30b4989cfff6257fff09b8f68f4aa294fad8b736
Noubar Jack Hanno <noubar@Noubars-MacBook-Pro-2.local>
2019-07-18 22:22:31 +0400

    SQL Database connection with mysql (server and workbench) and and add some pure sql queries to our application

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,24 +2,26 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll().then(([rows]) => {
     res.render('shop/product-list', {
-      prods: products,
+      prods: rows,
       pageTitle: 'All Products',
       path: '/products'
     });
-  });
+  }).catch(err => {
+    console.log(err);
+  })
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  Product.findById(prodId).then(([product]) => {
     res.render('shop/product-detail', {
-      product: product,
+      product: product[0],
       pageTitle: product.title,
       path: '/products'
     });
-  });
+  }).catch(err => {console.log(err)})
 };
 
 exports.getIndex = (req, res, next) => {
