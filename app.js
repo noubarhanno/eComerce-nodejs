c57f5d54c710958ab9a86be57fc1c7b078c3f4f9
Noubar Jack Hanno <noubar@Noubars-MacBook-Pro-2.local>
2019-07-22 19:08:26 +0400

    change the model method from sql to use sequelize and added some association like user connected to product and used the user.createProduct special method that has been created by sequelize when we add the association in app.js

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -17,9 +19,46 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1).then(user => {
+        req.user = user;
+        // this will attach the user object to each request
+        // and this user is not a user object fetched from the database
+        // it's a sequelize object that can execute a method on like destroy foe example
+        next();
+    }).catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
+//force: true will drop the database and create new one 
+// we do this because we add the constraints and these constraints will not be applied if the 
+// database and the tables already created , and this option should not be applied in the production
+// only in the development , beause we don't need to override the tables every time we restart our server
+sequelize
+    .sync()
+    .then(result => {
+        return User.findByPk(1)
+        
+}).then(user => {
+    if (!user){
+        return User.create({name: 'noubar', email: 'noubar@noubar.com'});
+    }
+    return Promise.resolve(User); // we wrapped the User with a promise that will be already added once we return from a promise function , each return
+    // already wrapped in promise function , and if we removed the promise function that's totaly fine
+}).then(() => {
+    app.listen(3000);
+})
+.catch(err => {
+    console.log(err);
+})
+
+
+// incoming request is only executed through middleware like app.use
+
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,9 +1,9 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/edit-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
     editing: false
   });
 };
@@ -14,30 +14,47 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save().then(() => {
-    res.redirect('/');
-  }).catch(err => {
-    console.log(err);
+  // we can use req.user.createProduct() instead of assigning the userId when we create the product because 
+  // of the association that we did , we linked the user table with the product table so sequelie has created a method automatically to create
+  // a product from the user table by using req.ueser.createProduct
+  req.user.createProduct({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description,
   })
+  // Product.create({
+  //   title: title,
+  //   price: price,
+  //   imageUrl: imageUrl,
+  //   description: description,
+  //   userId: req.user.id
+  // })
+    .then(() => {
+      res.redirect('/admin/products');
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  Product.findByPk(prodId).then(product => {
     if (!product) {
-      return res.redirect('/');
+      return res.redirect("/");
     }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
       editing: editMode,
       product: product
     });
-  });
+  })
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -46,29 +63,39 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  Product.findByPk(prodId).then(product => {
+    product.title = updatedTitle,
+    product.price = updatedPrice,
+    product.description = updatedDesc,
+    product.imageUrl = updatedImageUrl
+    return product.save();
+  }).then(() => res.redirect("/admin/products")).catch(err => {console.log(err)}); // we added the second .then because we returned the .save which is a promised method
+  // so catch here will catch the errors for both the first and the second promise 
+  
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
+  Product.findAll()
+    .then(products => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products"
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
-  });
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  // Product.destroy({where: { id: prodId }});
+  // instead we can use the other approach
+  Product.findByPk(prodId).then(product => {
+    return product.destroy();
+  }).then(() => {
+    res.redirect("/admin/products");
+  }).catch(err => console.log(err));
+  
 };
